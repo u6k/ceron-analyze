c954af14f6ddae42a2861a6c69614c8fabe8a6e8
u6k <u6k.apps@gmail.com>
2017-04-23 18:52:55 +0900

    Ceron「総合」カテゴリーをダウンロードして、ページ構造をチェックする処理を実装しました。

---
+++ src/main/java/me/u6k/ceron_analyze/service/CrawlerService.java
@@ -0,0 +1,105 @@
+
+package me.u6k.ceron_analyze.service;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import java.util.stream.Collectors;
+
+import me.u6k.ceron_analyze.util.NetworkUtil;
+import org.apache.commons.lang3.StringUtils;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CrawlerService {
+
+    private static final Logger L = LoggerFactory.getLogger(CrawlerService.class);
+
+    public void downloadAllCategory() {
+        L.info("#downloadAllCategory start");
+        try {
+            /*
+             * カテゴリーHTMLをダウンロード
+             */
+            URL allUrl = new URL("http://ceron.jp/all/");
+            String expectedTitle = "総合";
+
+            String html = NetworkUtil.get(allUrl);
+
+            /*
+             * HTMLを解析、想定している構造かチェック
+             */
+            Document htmlDoc = Jsoup.parse(html);
+
+            // 見出しをチェック
+            Elements elements = htmlDoc.select("h1.page_title");
+            if (elements.size() != 1) {
+                throw new RuntimeException("h1.page_title.size not 1.");
+            }
+            Element headE = elements.get(0);
+            if (!StringUtils.equals(headE.text(), expectedTitle)) {
+                throw new RuntimeException("head.text=" + headE.val() + " is not \"" + expectedTitle + "\".");
+            }
+
+            // 記事をチェック
+            Pattern commentPattern = Pattern.compile("^([0-9]+)コメント$");
+
+            elements = htmlDoc.select("div.item div.item_status span.link_num");
+            List<Integer> commentNumbers = elements.stream()
+                            .map(x -> {
+                                String text = x.text().trim();
+                                Matcher m = commentPattern.matcher(text);
+                                if (!m.find()) {
+                                    throw new RuntimeException("commentText=" + text + " not match.");
+                                }
+                                String commentCountText = m.group(1);
+                                int commentCount = Integer.parseInt(commentCountText);
+                                return commentCount;
+                            })
+                            .collect(Collectors.toList());
+
+            Pattern urlPattern = Pattern.compile("^\\/url\\/(.+)$");
+
+            elements = htmlDoc.select("div.item div.item_title a");
+            List<URL> urls = elements.stream()
+                            .map(x -> {
+                                String text = x.attr("href").trim();
+                                Matcher m = urlPattern.matcher(text);
+                                if (!m.find()) {
+                                    throw new RuntimeException("urlText=" + text + " not match.");
+                                }
+                                String path = m.group(1);
+                                URL url;
+                                try {
+                                    url = new URL("http://" + path);
+                                } catch (MalformedURLException e) {
+                                    throw new RuntimeException(e);
+                                }
+                                return url;
+                            })
+                            .collect(Collectors.toList());
+            List<String> titles = elements.stream()
+                            .map(x -> x.text().trim())
+                            .collect(Collectors.toList());
+
+            if (commentNumbers.size() != urls.size() || urls.size() != titles.size()) {
+                throw new RuntimeException("commentNumbers.size=" + commentNumbers.size()
+                                + ", urls.size=" + urls.size()
+                                + ", titles.size=" + titles.size() + " not equal.");
+            }
+        } catch (MalformedURLException e) {
+            throw new RuntimeException("html parse fail.", e);
+        } finally {
+            L.info("#downloadAllCategory finish");
+        }
+    }
+
+}
---
+++ src/test/java/me/u6k/ceron_analyze/service/CrawlerServiceTest.java
@@ -0,0 +1,22 @@
+
+package me.u6k.ceron_analyze.service;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class CrawlerServiceTest {
+
+    @Autowired
+    private CrawlerService service;
+
+    @Test
+    public void downloadAllCategory() throws Exception {
+        this.service.downloadAllCategory();
+    }
+
+}
