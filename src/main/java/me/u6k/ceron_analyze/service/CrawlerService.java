a8607ad05c8143a5b68c10d490b14c7e894430ee
u6k <u6k.apps@gmail.com>
2017-04-23 20:48:51 +0900

    ダウンロードしたhtmlをS3ストレージにアップロードするように変更しました。

--- src/main/java/me/u6k/ceron_analyze/service/CrawlerService.java
+++ src/main/java/me/u6k/ceron_analyze/service/CrawlerService.java
@@ -1,14 +1,18 @@
 
 package me.u6k.ceron_analyze.service;
 
+import java.io.IOException;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 import java.util.stream.Collectors;
 
 import me.u6k.ceron_analyze.util.NetworkUtil;
+import me.u6k.ceron_analyze.util.S3Util;
 import org.apache.commons.lang3.StringUtils;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
@@ -16,12 +20,16 @@ import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class CrawlerService {
 
     private static final Logger L = LoggerFactory.getLogger(CrawlerService.class);
+
+    @Autowired
+    private S3Util s3;
 
     public void downloadAllCategory() {
         L.info("#downloadAllCategory start");
@@ -30,7 +38,9 @@ public class CrawlerService {
              * カテゴリーHTMLをダウンロード
              */
             URL allUrl = new URL("http://ceron.jp/all/");
-            String expectedTitle = "総合";
+            String allTitle = "総合";
+            String allCategory = "all";
+            String downloadTimestamp = new SimpleDateFormat("yyyyMMdd-HHmmss").format(new Date());
 
             String html = NetworkUtil.get(allUrl);
 
@@ -45,8 +55,8 @@ public class CrawlerService {
                 throw new RuntimeException("h1.page_title.size not 1.");
             }
             Element headE = elements.get(0);
-            if (!StringUtils.equals(headE.text(), expectedTitle)) {
-                throw new RuntimeException("head.text=" + headE.val() + " is not \"" + expectedTitle + "\".");
+            if (!StringUtils.equals(headE.text(), allTitle)) {
+                throw new RuntimeException("head.text=" + headE.val() + " is not \"" + allTitle + "\".");
             }
 
             // 記事をチェック
@@ -95,7 +105,13 @@ public class CrawlerService {
                                 + ", urls.size=" + urls.size()
                                 + ", titles.size=" + titles.size() + " not equal.");
             }
-        } catch (MalformedURLException e) {
+
+            /*
+             * HTMLをS3ストレージにアップロード
+             */
+            String s3key = allCategory + "." + downloadTimestamp + ".html";
+            this.s3.putObject(s3key, html);
+        } catch (IOException e) {
             throw new RuntimeException("html parse fail.", e);
         } finally {
             L.info("#downloadAllCategory finish");
--- src/main/java/me/u6k/ceron_analyze/util/NetworkUtil.java
+++ src/main/java/me/u6k/ceron_analyze/util/NetworkUtil.java
@@ -32,25 +32,17 @@ public final class NetworkUtil {
             Thread.sleep(INTERVAL);
 
             HttpGet get = new HttpGet(url.toURI());
-            // get.addHeader("Accept-Encoding", "gzip");
 
             try (CloseableHttpResponse response = client.execute(get)) {
                 L.debug("response: status={}", response.getStatusLine());
                 if (response.getStatusLine().getStatusCode() != HttpStatus.SC_OK) {
                     throw new RuntimeException("url=" + url + ", statusCode=" + response.getStatusLine().getStatusCode());
                 }
-                // if (response.getHeaders("Content-Encoding").length == 0) {
-                // throw new RuntimeException("Content-Encoding header not found.");
-                // }
-                // if (!StringUtils.equals("gzip", response.getFirstHeader("Content-Encoding").getValue())) {
-                // throw new RuntimeException("content-encoding=" + response.getFirstHeader("Content-Encoding").getValue() + " is not gzip.");
-                // }
 
-                // HttpEntity entity = new GzipDecompressingEntity(response.getEntity());
                 HttpEntity entity = response.getEntity();
                 byte[] responseData = EntityUtils.toByteArray(entity);
                 String html = new String(responseData, StandardCharsets.UTF_8);
-                L.debug("response: body={}", html);
+                L.debug("response: body.length={}", html.length());
 
                 return html;
             }
