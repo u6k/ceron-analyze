4bb512c16074985edaec0381a83f49835d05b1a4
u6k <u6k.apps@gmail.com>
2017-04-23 15:59:33 +0900

    素のMavenプロジェクトを作成しました。"mvn package"が正常終了することを確認済み。

---
+++ src/main/java/me/u6k/ceron_analyze/App.java
@@ -0,0 +1,13 @@
+package me.u6k.ceron_analyze;
+
+/**
+ * Hello world!
+ *
+ */
+public class App
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
